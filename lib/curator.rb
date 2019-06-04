9c48586bf4333c1988d5de4f9d3f31d0e602de8f
Oscar Bellechasse <obellech10@gmail.com>
2019-06-04 10:56:16 -0600

    Create methods for find artist by id and find photo by id

--- lib/curator.rb
+++ lib/curator.rb
@@ -9,6 +9,23 @@ class Curator
 
   def add_photograph(photo)
     @photographs << Photograph.new(photo)
-    # binding.pry
   end
+
+  def add_artist(artist)
+    @artists << Artist.new(artist)
+  end
+
+  def find_artist_by_id(artist_id)
+    @artists.find do |artist|
+      artist if artist.id == artist_id
+    end
+  end
+
+  def find_photograph_by_id(photo_id)
+    @photographs.find do |photo|
+      photo if photo.id == photo_id
+    end
+  end
+
+
 end
