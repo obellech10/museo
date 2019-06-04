6c7736ce730d857ac16240878a3d6d54794a6add
Oscar Bellechasse <obellech10@gmail.com>
2019-06-04 10:55:44 -0600

    Add test for adding photographs

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -22,6 +22,20 @@ class CuratorTest < MiniTest::Test
       artist_id: "2",
       year: "1941"
     }
+    @artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    @artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
   end
 
   def test_that_it_exists
@@ -41,7 +55,17 @@ class CuratorTest < MiniTest::Test
     assert_equal boy, moon, @curator.photographs
     assert_equal boy.first, @curator.photographs.first
     assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @curator.photographs.first.name
+    assert_equal moon.last, @curator.find_photograph_by_id("2")
   end
 
-  
+  def test_it_can_add_artists
+    bresson = @curator.add_artist(@artist_1)
+    adams = @curator.add_artist(@artist_2)
+
+    assert_equal bresson, adams, @curator.artists
+    assert_equal bresson.first, @curator.artists.first
+    assert_equal "Henri Cartier-Bresson", @curator.artists.first.name
+    assert_equal bresson.first, @curator.find_artist_by_id("1")
+  end
+
 end
